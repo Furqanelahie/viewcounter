a59912c92d76e6b921d82ee759e693d879d95602
Furqanelahie <61836474+Furqanelahie@users.noreply.github.com>
2021-06-07 19:27:40 +0530

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,6 @@ $dataid=$_GET['id'];
 
  $json_object= file_get_contents('https://irreverent-hinge.000webhostapp.com/viewcounter/index.php?id=01');
 
-$data = json_decode($json_object, true);
 
-echo $data[$dataid];
+echo $json_object[$dataid];
 ?>
