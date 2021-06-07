880d33e324607efe6162ad1e3943d99ead6442e7
Furqanelahie <61836474+Furqanelahie@users.noreply.github.com>
2021-06-07 19:23:51 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
 <?php
- 
-$db=file_get_contents("https://irreverent-hinge.000webhostapp.com/viewcounter/index.php?id=01");
-echo $db;
+
+header('Access-Control-Allow-Origin: *');
+
+header( "Content-type: image/png" );
+
+$dataid=$_GET['id'];
+
+ $json_object= file_get_contents('https://irreverent-hinge.000webhostapp.com/viewcounter/index.php?id=01');
+
+$data = json_decode($json_object, true);
+
+echo $data[$dataid];
 ?>
